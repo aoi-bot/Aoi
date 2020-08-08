80090465ac1b67f71dd0722f5dd4f7e52d1ec5cc
crazygmr101 <dannash987@gmail.com>
2020-08-07 20:24:28 -0500

    Add gelbooru skeleton

--- aoi/aoi_bot.py
+++ aoi/aoi_bot.py
@@ -1,17 +1,22 @@
-from typing import Dict, Optional
+from typing import Dict, Optional, List
 
 import discord
 from discord.ext import commands
 import logging
+import os
 
 from .custom_context import AoiContext
 from .database import AoiDatabase
+
 
 class AoiBot(commands.Bot):
     def __init__(self, *args, **kwargs):
         super(AoiBot, self).__init__(*args, **kwargs)
         self.db: Optional[AoiDatabase] = None
         self.prefixes: Dict[int, str] = {}
+        self.banned_tags: List[str] = []
+        self.gelbooru_key: str = ""
+        self.gelbooru_user: str = ""
 
     async def on_message(self, message: discord.Message):
         ctx = await self.get_context(message, cls=AoiContext)
@@ -28,6 +33,9 @@ class AoiBot(commands.Bot):
         bot = kwargs.pop('bot', True)
         reconnect = kwargs.pop('reconnect', True)
         self.db = AoiDatabase()
+        self.banned_tags = os.getenv("BANNED_TAGS").split(",")
+        self.gelbooru_user = os.getenv("GELBOORU_USER")
+        self.gelbooru_key = os.getenv("GELBOORU_KEY")
         await self.db.load()
 
         if kwargs:
