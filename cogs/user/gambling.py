e8aa67544580faac734ba9b097309f5d1d5e661a
crazygmr101 <dannash987@gmail.com>
2020-08-27 01:09:12 -0500

    add betroll command

--- cogs/user/gambling.py
+++ cogs/user/gambling.py
@@ -1,7 +1,8 @@
-import discord
-from discord.ext import commands
-import aoi
 import random
+
+from discord.ext import commands
+
+import aoi
 
 
 class Gambling(commands.Cog):
@@ -28,6 +29,27 @@ class Gambling(commands.Cog):
             return await ctx.send_error("You must bet at least 5.")
         await ctx.send_info(f"You got **{ht}**. You {'win' if ht[0] == h_or_t.lower()[0] else 'lose'} ${bet:,}")
         await self.bot.db.award_guild_currency(ctx.author, bet if ht[0] == h_or_t.lower()[0] else -bet)
+
+    @commands.command(
+        brief="100 - 10x bet, >90 - x4 bet, >66 - x2 bet",
+        aliases=["br"]
+    )
+    async def betroll(self, ctx: aoi.AoiContext, bet: int):
+        if bet > await self.bot.db.get_guild_currency(ctx.author):
+            return await ctx.send_error("You don't have enough currency.")
+        if bet < 5:
+            return await ctx.send_error("You must bet at least 5.")
+        r = random.randint(0, 101)
+        if r == 100:
+            win = bet * 10
+        elif r > 90:
+            win = bet * 4
+        elif r > 66:
+            win = bet * 2
+        else:
+            win = 0
+        await ctx.send_info(f"You got a {r}. {'Better luck next time?' if not win else 'You won ' + str(win) + '!'}")
+        await self.bot.db.award_guild_currency(ctx.author, win-bet)
 
 
 def setup(bot: aoi.AoiBot) -> None:
