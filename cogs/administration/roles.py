9fddd981f4a150bc7f815a84cf3568e954ba38ff
crazygmr101 <dannash987@gmail.com>
2020-09-21 02:42:17 -0500

    add rolegrad ,delrole, and createrole commands

--- cogs/administration/roles.py
+++ cogs/administration/roles.py
@@ -1,5 +1,11 @@
+import io
+from typing import List
+
 import discord
+from PIL import Image
+from PIL import ImageDraw
 from discord.ext import commands
+from discord.ext.commands import Greedy
 
 import aoi
 from libs import conversions
@@ -57,6 +63,60 @@ class Roles(commands.Cog):
         await role.edit(colour=color)
         await ctx.send_info(f"Changed {role.mention}'s color to "
                             f"#{conversions.color_to_string(role.colour)}")
+
+    @commands.bot_has_permissions(manage_roles=True)
+    @commands.has_permissions(manage_roles=True)
+    @commands.command(brief="Creates one or more roles - rolenames must be separated by semicolons.",
+                      aliases=["cr"])
+    async def createrole(self, ctx: aoi.AoiContext, *, names: str):
+        roles = [await ctx.guild.create_role(name=name) for name in names.split(";")]
+        if len(roles) > 3:
+            conf = await ctx.confirm("Create roles: " + (" ".join(f"`{n}`" for n in roles) + "?"),
+                                     "Creating roles...",
+                                     "Role creation cancelled")
+            if not conf:
+                return
+        await ctx.send_ok(f"Created {' '.join(r.mention for r in roles)}")
+
+    @commands.bot_has_permissions(manage_roles=True)
+    @commands.has_permissions(manage_roles=True)
+    @commands.command(brief="Deletes one or more roles",
+                      aliases=["dr"])
+    async def deleterole(self, ctx: aoi.AoiContext, roles: Greedy[discord.Role]):
+        if len(roles) > 3:
+            conf = await ctx.confirm(f"Delete {' '.join(r.name for r in roles)}?",
+                                     "Deleting roles...",
+                                     "Role deletion cancelled")
+            if not conf:
+                return
+        await ctx.send_ok(f"Deleted {' '.join('`' + r.name + '`' for r in roles)}")
+
+    @commands.bot_has_permissions(manage_roles=True)
+    @commands.has_permissions(manage_roles=True)
+    @commands.command(
+        brief="Colors roles as an RGB gradient between colors",
+        aliases=["rolegrad"]
+    )
+    async def rolegradient(self, ctx: aoi.AoiContext, color1: discord.Colour, color2: discord.Colour,
+                           roles: Greedy[discord.Role]):
+        roles: List[discord.Role] = list(roles)
+        num = len(roles)
+        rgb, rgb2 = color1.to_rgb(), color2.to_rgb()
+        steps = [(rgb[x] - rgb2[x]) / (num - 1) for x in range(3)]
+        colors = list(reversed([tuple(map(int, (rgb2[x] + steps[x] * n for x in range(3)))) for n in range(num)]))
+        img = Image.new("RGB", (240, 48))
+        img_draw = ImageDraw.Draw(img)
+        for n, clr in enumerate(colors):
+            await roles[n].edit(color=int("".join(hex(x)[2:] for x in clr), 16))
+            img_draw.rectangle([
+                (n * 240 / num, 0),
+                ((n + 1) * 240 / num, 48)
+            ], fill=tuple(map(int, clr)))
+        buf = io.BytesIO()
+        img.save(buf, format="PNG")
+        await ctx.embed(title="Roles colored according to gradient",
+                        description=" ".join("#" + "".join(hex(x)[2:] for x in c) for c in colors),
+                        image=buf)
 
 
 def setup(bot: aoi.AoiBot) -> None:
