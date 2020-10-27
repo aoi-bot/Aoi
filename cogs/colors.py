a66a073cb32eead3c776616eeeddb06d0efa544e
crazygmr101 <dannash987@gmail.com>
2020-10-27 01:48:29 -0500

    fix gradient command

--- cogs/colors.py
+++ cogs/colors.py
@@ -90,10 +90,12 @@ class Colors(commands.Cog):
     )
     async def gradient(self, ctx: aoi.AoiContext, color1: AoiColor, color2: AoiColor, num: int = 4):
         if num < 3 or num > 10:
-            return await ctx.send_error("Number of colors must be between 2 and 10")
+            return await ctx.send_error("Number of colors must be between 3 and 10")
         rgb, rgb2 = color1.to_rgb(), color2.to_rgb()
         steps = [(rgb[x] - rgb2[x]) / (num - 1) for x in range(3)]
-        colors = list(reversed([tuple(map(int, (rgb2[x] + steps[x] * n for x in range(3)))) for n in range(num + 1)]))
+        colors = list(reversed([
+            tuple(map(int, (rgb2[x] + steps[x] * n for x in range(3)))) for n in range(num + 1)
+        ]))[1:]
         img = Image.new("RGB", (240, 48))
         img_draw = ImageDraw.Draw(img)
         for n, clr in enumerate(colors):
@@ -104,7 +106,7 @@ class Colors(commands.Cog):
         buf = io.BytesIO()
         img.save(buf, format="PNG")
         await ctx.embed(title="Gradient",
-                        description=" ".join("#" + "".join(hex(x)[2:] for x in c) for c in colors[1:]),
+                        description=" ".join("#" + "".join(hex(x)[2:].rjust(2, "0") for x in c) for c in colors),
                         image=buf)
 
     @commands.max_concurrency(1, commands.BucketType.user)
