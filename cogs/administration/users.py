9e788d410075195c00b132300c80f33f6fe8cd7d
crazygmr101 <dannash987@gmail.com>
2020-10-30 02:39:22 -0500

    allow users to set aoi's nickname

--- cogs/administration/users.py
+++ cogs/administration/users.py
@@ -16,11 +16,14 @@ class Users(commands.Cog):
     @commands.bot_has_permissions(manage_nicknames=True)
     @commands.command(brief="Set a member's nickname")
     async def setnick(self, ctx: aoi.AoiContext, member: discord.Member, *, nickname: str):
-        if member.top_role >= ctx.me.top_role:
-            raise aoi.RoleHierarchyError("I can't change the nickname of a person with a role higher than mine!")
         if member.top_role >= ctx.author.top_role:
             raise aoi.RoleHierarchyError("You can't change the nickname of a person with a role higher than yours!")
-        await member.edit(nick=nickname)
+        if member.id == ctx.me.id:
+            await ctx.me.edit(nick=nickname)
+        else:
+            if member.top_role >= ctx.me.top_role:
+                raise aoi.RoleHierarchyError("I can't change the nickname of a person with a role higher than mine!")
+            await member.edit(nick=nickname)
         await ctx.send_ok(f"{member.mention}'s nickname set to {nickname}")
 
 
