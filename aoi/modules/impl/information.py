7d6c5609cac757b9f1b58ef2a20deb89f3387186
crazygmr101 <dannash987@gmail.com>
2021-10-31 16:19:35 -0400

    FIxed some things :P

--- aoi/modules/impl/information.py
+++ aoi/modules/impl/information.py
@@ -9,12 +9,18 @@ def get_color(member: hikari.Member):
         return hikari.Color.of(0x000000)
 
 
-def get_role_members(ctx: tanjun.abc.Context, role: hikari.Role):
-    members = []
-    for mem in ctx.get_guild().get_members():
-        if role in mem.get_roles():
-            members.append(mem)
-    return members
+# TODO delete this
+# forgot to typehint
+# role_ids looks to be already cached, so no need to call get_roles()
+# get_members() actually returns {snowflake: member, snowflake: member}, hence the values()
+
+def get_role_members(ctx: tanjun.abc.Context, role: hikari.Role) -> list[hikari.Member]:
+    return [mem for mem in ctx.get_guild().get_members().values() if role.id in mem.role_ids]
+
+
+def get_role_member_count(ctx: tanjun.abc.Context, role: hikari.Role) -> int:
+    return sum(1 if role.id in member.role_ids else 0
+               for member in ctx.get_guild().get_members().values())
 
 
 async def avatar(ctx: tanjun.abc.Context, member: hikari.Member):
@@ -23,19 +29,19 @@ async def avatar(ctx: tanjun.abc.Context, member: hikari.Member):
 
     await ctx.respond(
         embed=hikari.Embed(title=f"{member}'s Avatar", color=get_color(member))
-        .set_image(member.avatar_url)
-        .set_footer(text=f"ID: {member.id}")
+            .set_image(member.avatar_url)
+            .set_footer(text=f"ID: {member.id}")
     )
 
 
 async def roleinfo(ctx: tanjun.abc.Context, role: hikari.Role):
     await ctx.respond(
         embed=hikari.Embed(title=f"Info for {role}", color=role.color)
-        .add_field(name="ID", value=str(role.id))
-        .add_field(name="Members", value=str(get_role_members(ctx, role)))
-        .add_field(name="Color", value=str(role.color))
-        .add_field(name="Hoisted", value=str(role.is_hoisted))
-        .add_field(name="Mentionable", value=str(role.is_mentionable))
-        .add_field(name="Position", value=str(role.position))
-        .add_field(name="Created at", value=str(role.created_at.strftime("%c")))
+            .add_field(name="ID", value=str(role.id), inline=True)
+            .add_field(name="Members", value=str(get_role_member_count(ctx, role)), inline=True)
+            .add_field(name="Color", value=str(role.color), inline=True)
+            .add_field(name="Hoisted", value=str(role.is_hoisted), inline=True)
+            .add_field(name="Mentionable", value=str(role.is_mentionable), inline=True)
+            .add_field(name="Position", value=str(role.position), inline=True)
+            .add_field(name="Created at", value=str(role.created_at.strftime("%c")), inline=True)
     )
